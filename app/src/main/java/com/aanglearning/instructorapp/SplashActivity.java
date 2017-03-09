65bfacd01bd3a791584e39d4605f95622a9a5b1e
vinkrish <vinaykrishna1989@gmail.com>
2017-03-09 12:18:42 +0530

    project setup with necessary library

---
+++ app/src/main/java/com/aanglearning/instructorapp/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.aanglearning.instructorapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aanglearning.instructorapp.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
---
+++ app/src/main/java/com/aanglearning/instructorapp/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.aanglearning.instructorapp.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aanglearning.instructorapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
